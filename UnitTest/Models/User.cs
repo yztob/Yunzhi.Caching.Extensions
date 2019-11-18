d86f961a40cec27b93e5c304f2188dbdd50ddb5f
alphaair <alphaair@163.com>
2019-11-18 10:41:40 +0800

    #add 添加单元测试代码。

---
+++ UnitTest/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Models
+{
+    /// <summary>
+    /// 用户测试
+    /// </summary>
+    public class User
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
